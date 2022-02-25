38671ef8a3a4c7413d79e6a244dc76c30af344b9
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-02-25 16:21:43 +0000

    Re-enable WebGLAddon and remove redundant package

--- src/app/OutputTerminal.tsx
+++ src/app/OutputTerminal.tsx
@@ -111,7 +111,6 @@ export const OutputTerminal = ({setXTerm}: OutputTerminalProps) => {
 		setXTerm(newTerm);
 		newTerm.open(xtermDiv.current);
 
-		/*
 		let isWebglEnabled = false;
 		try {
 			newTerm.loadAddon(new WebglAddon());
@@ -119,7 +118,6 @@ export const OutputTerminal = ({setXTerm}: OutputTerminalProps) => {
 		} catch (e) {
 			console.warn("WebGL addon threw an exception during load", e);
 		}
-		*/
 
 		const fitAddon = new FitAddon();
 		newTerm.loadAddon(fitAddon);
